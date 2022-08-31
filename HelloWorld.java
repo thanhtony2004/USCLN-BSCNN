e74c7143a41fe0256ab1905e50e51ede5544070d
thanhtony2004 <tranthanh2k4dx@gmail.com>
2022-08-31 07:32:51 +0700

    tim USCLN va BSCNN

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,36 +1,29 @@
 import java.util.Scanner;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("please enter a number 0 till 1000!");
-        System.out.println("we'll check and print every prime number 0 till 1000");
-        //gan mac dinh prime la false
-        boolean isPrime ;
-        int a = scanner.nextInt();
-
-        if(a>2&&a<1000){
-            //mac dinh so 1 ,2 la so nguyen to ko can check
-            System.out.print(1+" ");
-            System.out.print(2+" ");
-
-            for (int i=3;i<=a;i++){
-                //gan true neu i la so nguyen to
-                isPrime =true;
-                for (int j =2;j<i;j++){
-                    //check i neu ko la so nguyen to gan false
-                    if(i%j==0){
-                        isPrime =false;
-                    }
-                }
-                //in ra so nguyen to tu 0->a
-                if (isPrime){
-                    System.out.print(i+" ");
-                }
+    public static int USCLN(int a , int b){
+        while (a!=b){
+            if(a>b){
+                a-=b;
+            } else {
+                b-=a;
             }
-        } else {
-            System.out.println("so nhap khong hop le");
         }
+        return a;
+    }
+    public static int BSCNN(int a, int b){
+        return a*b/USCLN(a,b);
+    }
+    public static void main(String[] args) {
+        System.out.println("tinh USCLN va BSCNN");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap vao so a");
+        int a = scanner.nextInt();
+        System.out.println("nhap vao so b");
+        int b = scanner.nextInt();
+        System.out.println("USCLN : " + USCLN(a,b));
+        System.out.println("BSCNN : " + BSCNN(a,b));
 
     }
+
 }
